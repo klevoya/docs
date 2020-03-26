1a2017e49ae6a3a35aafadf6c883b89e6cf43329
MrToph <christoph.michel.1992@gmail.com>
2020-03-26 14:17:56 +0400

    Add base url to redirect

--- src/pages/hydra.js
+++ src/pages/hydra.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import {Redirect} from '@docusaurus/router';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import useBaseUrl from '@docusaurus/useBaseUrl';
 
 function Hydra() {
   const context = useDocusaurusContext();
+  const firstDocUrl = useBaseUrl('hydra/about/getting-started')
   const {siteConfig = {}} = context;
   
   // redirect to first entry
   return (
-    <Redirect to="/hydra/about/getting-started" />
+    <Redirect to={firstDocUrl} />
   );
 }
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import {Redirect} from '@docusaurus/router';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import useBaseUrl from '@docusaurus/useBaseUrl';
 
-function Hydra() {
+function Home() {
   const context = useDocusaurusContext();
+  const firstDocUrl = useBaseUrl('hydra/about/getting-started')
   const {siteConfig = {}} = context;
   
   // redirect to first entry
   return (
-    <Redirect to="/hydra/about/getting-started" />
+    <Redirect to={firstDocUrl} />
   );
 }
 
-export default Hydra;
+export default Home;
